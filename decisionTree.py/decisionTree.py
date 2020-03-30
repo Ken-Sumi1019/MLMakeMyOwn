4e5233f8a8961d03a55dacf10e60d6edf120e0d8
Ken-Sumi1019 <ken.sumi1019@gmail.com>
2020-03-31 07:34:09 +0900

    決定木の分岐閾値を見つける関数を実装

--- decisionTree.py/decisionTree.py
+++ decisionTree.py/decisionTree.py
@@ -10,6 +10,7 @@ class DecisionTree:
         self.data = np.array(data)
         self.label = np.array(label)
         self.categorys = np.unique(self.label)
+        self.tree = []
 
     """木を構築する"""
     def makeTree(self):
@@ -25,9 +26,28 @@ class DecisionTree:
         return 1.0 - sum([np.count_nonzero(self.label[index] == c) / n for c in self.categorys])
 
     """最良の閾値を見つける"""
-    def bestThreshold(self):
-        pass
+    def bestThreshold(self,indexs):
+        bestScore = 100
+        bestRight = [];bestLeft = []
+        bestIdx = 0;bestFeature = 0
+        for i in range(np.shape(self.data)[1]):
+            for j in indexs:
+                l,r = self.splitGroup(j,i,indexs)
+                lgini = self.giniScore(l);rgini = self.giniScore(r)
+                if bestScore > lgini + rgini:
+                    bestIdx = j;bestFeature = i
+                    bestRight = r;bestLeft = l
+                    bestScore = lgini + rgini
+        result = {"index" : bestIdx,"feature" : bestFeature,
+                  "right" : bestRight,"left" : bestLeft}
+        return result
 
     """指定した閾値に合わせてグループを分ける"""
-    def splitGroup(self):
-        pass
+    def splitGroup(self,index,feature,indexs):
+        right = [];left = []
+        for i in indexs:
+            if self.data[i][feature] < self.data[index][feature]:
+                left.append(i)
+            else:
+                right.append(i)
+        return left,right
